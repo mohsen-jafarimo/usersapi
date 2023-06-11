bd0cd67b0c5ae3c4f764b82768fe8193abfc45b0
mohsen <mohsen@gmail.com>
2023-06-11 22:08:10 +0430

    change home and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="shadow-md p-5 flex justify-center fixed bottom-0 right-0 left-0">
+      <h3 className="mx-3">Mohsen Jafari</h3>
       <p>Copyright &copy; 2023 </p>
     </footer>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   return (
     <div className="w-full h-ful p-20  flex-col flex  items-center">
       <div className="flex w-full h-full flex-col justify-center items-center mt-14">
-        <h3 className="text-2xl mb-4 ">Authentication</h3>
+        <h3 className="text-2xl mb-4 ">
+          {" "}
+          {userInfo ? " Welcome Back" : "Authentication"}
+        </h3>
         <div className="flex justify-between">
-          <button
-            onClick={() => navigate("/register")}
-            className="bg-blue-400 p-3 rounded-md text-white font-serif font-bold mx-1"
-          >
-            Register
-          </button>
-          <button
-            onClick={() => navigate("/login")}
-            className="bg-blue-400 p-3  rounded-md text-white font-serif font-bold"
-          >
-            Login
-          </button>
+          {userInfo ? (
+            <>
+              <h3 className="mx-4">{userInfo.email}</h3>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => navigate("/register")}
+                className="bg-blue-400 p-3 rounded-md text-white font-serif font-bold mx-1"
+              >
+                Register
+              </button>
+              <button
+                onClick={() => navigate("/login")}
+                className="bg-blue-400 p-3  rounded-md text-white font-serif font-bold"
+              >
+                Login
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
